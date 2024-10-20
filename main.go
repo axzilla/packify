55fbada6ad74ef48dd06a53f004c723e88aaaa51
“axzilla” <hello@axeladrian.com>
2024-10-20 16:58:18 +0200

    feat: first tree creator

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io/fs"
+	"os"
+	"strings"
+)
+
+type TreePoint struct {
+	name        string
+	isDirectory bool
+	children    []string
+}
 
 func main() {
-	fmt.Println("hello world")
+	file, err := os.Create("packify.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	defer writer.Flush()
+
+	err = fs.WalkDir(os.DirFS("."), ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		layers := strings.Count(path, "/")
+		for range layers {
+			_, err := writer.WriteString(" ")
+			if err != nil {
+				return err
+			}
+		}
+
+		_, err = writer.WriteString(path + "\n")
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
 }
